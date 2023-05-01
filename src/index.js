ae8353fe4d4bdd6ddc47c98c228e7794f90a59e3
Pavel-Poliakov <polaykovp.s@gmail.com>
2023-05-02 00:08:24 +0400

    fix: js file for branch gh-pages

--- src/index.js
+++ src/index.js
@@ -43,18 +43,42 @@ const Keyboard = {
     this.elements.main.appendChild(this.elements.textarea);
     this.elements.main.appendChild(this.elements.keysContainer);
     this.elements.keysContainer.appendChild(this.createKeys());
+    document.querySelectorAll('.textarea').forEach((element) => {
+      element.addEventListener('focus', () => {
+        this.open(element.value, (currentValue) => {
+          // eslint-disable-next-line no-param-reassign
+          element.value = currentValue;
+        });
+      });
+    });
   },
 
   createKeys() {
     const fragment = document.createDocumentFragment();
-    const arrWord = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete', 'caps lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '; ', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '', 'Shift', 'ctrl', 'win', 'alt', 'Space', 'alt', '', '', '', 'ctrl'];
+    const arrWord = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete', 'caps lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '; ', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Up', 'Shift', 'ctrl', 'win', 'alt', 'Space', 'alt', 'Left', 'Down', 'Right', 'ctrl'];
 
     // eslint-disable-next-line camelcase
     const createIcon = (icon_name) => `<i class="material-icons">${icon_name}</i>`;
-    arrWord.forEach((key) => {
+
+    arrWord.forEach((key, index) => {
       const button = document.createElement('button');
       button.setAttribute('type', 'button');
       button.classList.add('btn');
+      index === 42 ? button.classList.add('btn-shift-left') : false;
+      index === 53 ? button.classList.add('btn-up') : false;
+      index === 53 ? button.setAttribute('btnname', 'ArrowUp') : false;
+      index === 54 ? button.classList.add('btn-shift-right') : false;
+      index === 55 ? button.classList.add('btn-ctrl-left') : false;
+      index === 56 ? button.setAttribute('btnname', 'Meta') : false;
+      index === 57 ? button.classList.add('btn-alt-left') : false;
+      index === 59 ? button.classList.add('btn-alt-right') : false;
+      index === 60 ? button.classList.add('btn-left') : false;
+      index === 60 ? button.setAttribute('btnname', 'ArrowLeft') : false;
+      index === 61 ? button.classList.add('btn-down') : false;
+      index === 61 ? button.setAttribute('btnname', 'ArrowDown') : false;
+      index === 62 ? button.classList.add('btn-right') : false;
+      index === 62 ? button.setAttribute('btnname', 'ArrowRight') : false;
+      index === 63 ? button.classList.add('btn-ctrl-right') : false;
 
       // eslint-disable-next-line default-case
       switch (key) {
@@ -78,9 +102,7 @@ const Keyboard = {
             this.toggleCapsLock();
             button.classList.toggle('active', this.properties.capsLock);
           });
-
           break;
-
         case 'Enter':
           button.classList.add('btn-enter');
           button.innerHTML = createIcon('Enter');
@@ -89,9 +111,7 @@ const Keyboard = {
             this.properties.value += '\n';
             this.triggerEvent('oninput');
           });
-
           break;
-
         case 'Space':
           button.classList.add('btn-space');
           button.innerHTML = createIcon('Space');
@@ -100,20 +120,40 @@ const Keyboard = {
             this.properties.value += ' ';
             this.triggerEvent('oninput');
           });
-
           break;
-
+        case 'Tab':
+          button.classList.add('btn-tab');
+          button.innerHTML = createIcon('Tab');
+          button.addEventListener('click', () => {
+            this.properties.value += '  ';
+            this.triggerEvent('oninput');
+          });
+          break;
+        case 'Delete':
+          button.classList.add('btn-del');
+          button.innerHTML = createIcon('Delete');
+          button.addEventListener('click', () => {
+            // eslint-disable-next-line max-len
+            this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+            this.triggerEvent('oninput');
+          });
+          break;
         case 'Shift':
           button.classList.add('btn-shift');
           button.innerHTML = createIcon('Shift');
-
-          button.addEventListener('click', () => {
-            this.properties.value += ' ';
-            this.triggerEvent('oninput');
-          });
-
           break;
-
+        case 'ctrl':
+          button.classList.add('btn-ctrl');
+          button.innerHTML = createIcon('ctrl');
+          break;
+        case 'alt':
+          button.classList.add('btn-alt');
+          button.innerHTML = createIcon('alt');
+          break;
+        case 'win':
+          button.classList.add('btn-win');
+          button.innerHTML = createIcon('win');
+          break;
         default:
           button.textContent = key.toLowerCase();
           button.addEventListener('click', () => {
@@ -121,18 +161,46 @@ const Keyboard = {
             this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
             this.triggerEvent('oninput');
           });
-
           break;
       }
       fragment.appendChild(button);
     });
     return fragment;
   },
-
+  triggerEvent(handlerName) {
+    if (typeof this.eventHandlers[handlerName] === 'function') {
+      this.eventHandlers[handlerName](this.properties.value);
+    }
+  },
+  toggleCapsLock() {
+    this.properties.capsLock = !this.properties.capsLock;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key of this.elements.keys) {
+      if (key.childElementCount === 0) {
+        // eslint-disable-next-line max-len
+        key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+      }
+    }
+  },
+  open(initialValue, oninput, onclose) {
+    this.properties.value = initialValue || '';
+    this.eventHandlers.oninput = oninput;
+    this.eventHandlers.onclose = onclose;
+  },
+  close() {
+    this.properties.value = '';
+    this.eventHandlers.oninput = oninput;
+    this.eventHandlers.onclose = onclose;
+  },
 };
 window.addEventListener('DOMContentLoaded', () => {
   Keyboard.createDom();
 });
+
+
+
+
+
 // const body = document.querySelector('body');
 // const header = document.createElement('header');
 // const p = document.createElement('p');
@@ -192,80 +260,76 @@ window.addEventListener('DOMContentLoaded', () => {
 // // const btn = document.querySelectorAll('.btn');
 // // const btn = document.querySelectorAll('.btn');
 // // eslint-disable-next-line no-unused-vars
-// for (let i = 0; i < btn.length; i++) {
-//   btn[i].setAttribute('btnname', btn[i].innerText);
-//   btn[i].setAttribute('lowerCaseName', btn[i].innerText.toLowerCase());
-//   btn[53].setAttribute('btnname', 'ArrowUp');
-//   btn[56].setAttribute('btnname', 'Meta');
-//   btn[60].setAttribute('btnname', 'ArrowLeft');
-//   btn[61].setAttribute('btnname', 'ArrowDown');
-//   btn[62].setAttribute('btnname', 'ArrowRight');
-// }
-// window.addEventListener('keydown', (e) => {
-//   for (let i = 0; i < btn.length; i++) {
-//     if (e.key === btn[i].getAttribute('btnname') || e.key === btn[i].getAttribute('lowerCaseName')) {
-//       btn[i].classList.add('active');
-//     }
-//     if (e.code === 'Space') {
-//       space.classList.add('active');
-//     }
-//     if (e.code === 'ControlLeft') {
-//       ctrlLeft.classList.add('active');
-//     }
-//     if (e.code === 'ControlRight') {
-//       ctrlRight.classList.add('active');
-//     }
-//     if (e.code === 'AltLeft') {
-//       altLeft.classList.add('active');
-//     }
-//     if (e.code === 'AltRight') {
-//       altRight.classList.add('active');
-//     }
-//     if (e.code === 'ShiftLeft') {
-//       btnShiftRight.classList.remove('active');
-//     }
-//     if (e.code === 'ShiftRight') {
-//       btnShiftLeft.classList.remove('active');
-//     }
-//     // if (e.code === 'CapsLock') {
-//     //   capsLock.classList.add('active');
-//     // }
-//   }
-// });
-// window.addEventListener('keyup', (e) => {
-//   for (let i = 0; i < btn.length; i++) {
-//     if (e.key === btn[i].getAttribute('btnname') || e.key === btn[i].getAttribute('lowerCaseName')) {
-//       btn[i].classList.remove('active');
-//       btn[i].classList.add('remove');
-//     }
-//     if (e.code === 'Space') {
-//       space.classList.remove('active');
-//     }
-//     if (e.code === 'ControlLeft') {
-//       ctrlLeft.classList.remove('active');
-//     }
-//     if (e.code === 'ControlRight') {
-//       ctrlRight.classList.remove('active');
-//     }
-//     if (e.code === 'AltLeft') {
-//       altLeft.classList.remove('active');
-//     }
-//     if (e.code === 'AltRight') {
-//       altRight.classList.remove('active');
-//     }
-//     if (e.code === 'ShiftLeft') {
-//       btnShiftRight.classList.remove('active');
-//       btnShiftRight.classList.remove('remove');
-//     }
-//     if (e.code === 'ShiftRight') {
-//       btnShiftLeft.classList.remove('active');
-//       btnShiftLeft.classList.remove('remove');
-//     }
-//     setTimeout(() => {
-//       btn[i].classList.remove('remove');
-//     }, 100);
-//   }
-// });
+const btn = document.querySelectorAll('.btn');
+for (let i = 0; i < btn.length; i++) {
+  btn[i].setAttribute('btnname', btn[i].innerText);
+  btn[i].setAttribute('lowerCaseName', btn[i].innerText.toLowerCase());
+}
+window.addEventListener('keydown', (e) => {
+  for (let i = 0; i < btn.length; i++) {
+    if (e.key === btn[i].getAttribute('btnname') || e.key === btn[i].getAttribute('lowerCaseName')) {
+      btn[i].classList.add('active');
+    }
+    if (e.code === 'Space') {
+      space.classList.add('active');
+    }
+    if (e.code === 'ControlLeft') {
+      ctrlLeft.classList.add('active');
+    }
+    if (e.code === 'ControlRight') {
+      ctrlRight.classList.add('active');
+    }
+    if (e.code === 'AltLeft') {
+      altLeft.classList.add('active');
+    }
+    if (e.code === 'AltRight') {
+      altRight.classList.add('active');
+    }
+    if (e.code === 'ShiftLeft') {
+      btnShiftRight.classList.remove('active');
+    }
+    if (e.code === 'ShiftRight') {
+      btnShiftLeft.classList.remove('active');
+    }
+    // if (e.code === 'CapsLock') {
+    //   capsLock.classList.add('active');
+    // }
+  }
+});
+window.addEventListener('keyup', (e) => {
+  for (let i = 0; i < btn.length; i++) {
+    if (e.key === btn[i].getAttribute('btnname') || e.key === btn[i].getAttribute('lowerCaseName')) {
+      btn[i].classList.remove('active');
+      btn[i].classList.add('remove');
+    }
+    if (e.code === 'Space') {
+      space.classList.remove('active');
+    }
+    if (e.code === 'ControlLeft') {
+      ctrlLeft.classList.remove('active');
+    }
+    if (e.code === 'ControlRight') {
+      ctrlRight.classList.remove('active');
+    }
+    if (e.code === 'AltLeft') {
+      altLeft.classList.remove('active');
+    }
+    if (e.code === 'AltRight') {
+      altRight.classList.remove('active');
+    }
+    if (e.code === 'ShiftLeft') {
+      btnShiftRight.classList.remove('active');
+      btnShiftRight.classList.remove('remove');
+    }
+    if (e.code === 'ShiftRight') {
+      btnShiftLeft.classList.remove('active');
+      btnShiftLeft.classList.remove('remove');
+    }
+    setTimeout(() => {
+      btn[i].classList.remove('remove');
+    }, 100);
+  }
+});
 // // eslint-disable-next-line consistent-return
 // capsLock.addEventListener('click', () => {
 //   if (capsLock.classList.contains('active2')) {
